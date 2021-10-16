53881a6764b8734f6ea852b49b02e57f040bd83b
Franklin Bitencourt <you@example.com>
2021-10-16 14:41:29 -0300

    Add: dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const controller = require('./controllers');
+const dotenv = require('dotenv').config();
 
+const {PORT} = process.env;
 const app = express();
 app.use(express.json());
 
 app.get('/', controller.metadata);
 
-app.listen('3000', () => {
-    console.log('Servidor rodando na porta 3000');
+app.listen(PORT || 3000, () => {
+    console.log(`Servidor rodando na porta ${PORT || 3000}`);
 });
