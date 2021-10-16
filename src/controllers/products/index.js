a024c6b6a6f1148d19e063501d597c3d05c36780
Franklin Bitencourt <you@example.com>
2021-10-16 18:25:11 -0300

    Add : products categories filter

--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -1,7 +1,8 @@
 const { productsModel } = require('../../models');
 
 const get = async (req,res,next) => {
-    const products = await productsModel.getProducts();
+    const categories = req.query["categories"]?.split(",") ?? [];
+    const products = await productsModel.getProducts(categories);
     
     if (products.isError) {
         res.status(400).json(products);
--- src/models/products/index.js
+++ src/models/products/index.js
@@ -1,16 +1,21 @@
 const {connection} = require('../../configs');
 
-const getProducts = async () => {
+const getProducts = async (categories) => {
   const conn = await connection();
   try{
-    const [results] = await conn.execute(
+    let query = 
       'SELECT '+
       'p.id as `product_id`, p.name as `product_name`,  p.image as `product_image`,  p.description as `product_description`,'  +
       'p.composition as `product_composition`, p.skin_type as `product_skin_type`,p.price as `product_price`,'+
       'c.id as `category_id`, c.name as `category_name` ' +
       'FROM products  as `p` ' +
-      'LEFT JOIN categories as `c` ON p.category_id= c.id'
-    );
+      'LEFT JOIN categories as `c` ON p.category_id= c.id';
+    
+    if(categories.length > 0){
+      query += ` WHERE c.id IN(${categories.join(',')})` 
+    }
+    
+    const [results] = await conn.execute(query);
     return results;
   }
   catch(e)
